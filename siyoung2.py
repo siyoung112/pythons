c9f3bba455a010ba50f7d9434f91dc354e72edec
siyoung112 <67949348+siyoung112@users.noreply.github.com>
2023-03-12 15:46:26 +0900

    Update siyoung2.py

--- siyoung2.py
+++ siyoung2.py
@@ -1,10 +1,9 @@
 import requests
 import sys
+sys.stdout=open('siyoung2.txt','w')
 
 url="https://api.thecatapi.com/v1/images/search"
 data=requests.get(url).json()
-print(type(data))
-print(data)
 
-sys.stdout=open('siyoung2.txt','w')
+print(data)
 sys.stdout.close()
